b3e173612ec7b6f7ce6889c673ce916e1d0d4922
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-22 13:41:40 +0530

    Maniuplated Data

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const repos= document.getElementById("getRepo")
 repos.addEventListener("click", gRep)
 async function gRep() {
-    const url="https://api.github.com/search/repositories?q=stars:>10"
+    const url="https://api.github.com/search/repositories?q=stars:>300000"
     const response= await fetch(url)
-    console.log(response.json())
+    const divRes=document.getElementById("divRes")
+    const result=await response.json()
+    console.log(result)
+    result.items.forEach(i=>{
+        const anchor=document.createElement("a")
+        anchor.href=i.html_url
+        anchor.textContent=i.full_name
+        divRes.appendChild(anchor)
+        divRes.appendChild(document.createElement("br"))}
+
+    )
+
 }
