5b242bb13ff6bec99b2a7f8d77b012c2e6fe41bd
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-22 13:15:14 +0530

    Data Fetched Without Errors

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const repos= document.getElementById("getRepo")
 repos.addEventListener("click", gRep)
 async function gRep() {
-    a
-
+    const url="https://api.github.com/search/repositories?q=stars:>10"
+    const response= await fetch(url)
+    console.log(response.json())
 }
