346ea09c7a0328a68fee10f74f1fe1678df9d0b8
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-25 21:38:14 +0530

    Added More Views and URL Conf for the same

--- Tutorial/polls/views.py
+++ Tutorial/polls/views.py
@@ -7,3 +7,14 @@ def index(request):
 def trialView(request):
     return HttpResponse("This is a Trial View")
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
