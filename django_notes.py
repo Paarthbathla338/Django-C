dfbec32300c31a1a1fdae09cbe7a4c604e3b464c
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-25 21:22:01 +0530

    Admin User Created

--- django_notes.py
+++ django_notes.py
@@ -4,6 +4,7 @@
 # urls.py: Contains the Urls for your project. Just like Table of Contents Files
 # python manage.py runserver: To run the Django Server
 # python manage.py startapp : To make an App. Add an App name after startup. Be sure that it is in the Folder where your Manage.py is
+# py manage.py createsuperuser: To create a Admin User
 
 # {View Commands and Files}
 # View: A View in Django is a Callable which takes a request and returns as response. To Call a View you need to add the view as a callable in the URLS config. ie the urls.py file.
